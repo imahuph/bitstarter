e68dd8f7929591b4d3bda39dea8821a46ce3ded6
Ira Huff <imahuph@imahuph.net>
2013-07-06 14:56:34 -0700

    grader updated

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 var fs = require('fs');
 var program = require('commader');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -33,8 +34,13 @@ var assertFileExists = function(infile) {
 	}
     return instr;
 };
-
-var cheerioHtmlFile = function(htmlfile, checksfile) {
+var cheeroHtmlFile = function(htmlfile0 {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+var loadChecks = function(checksfile) {
+    return JSON.pars(fs.readFileSync(checksfile));
+};
+var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -44,13 +50,30 @@ var cheerioHtmlFile = function(htmlfile, checksfile) {
 	}
     return out;
 };
-
+var clone = function(fn) {
+    retrun fn.bind({});
+};
 if(require.main == module) {
     program
-    .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
-    .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+    .option('-c, --checks ', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+    .option('-f, --file ', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url ', 'Path to url')
     .parse(process.argv);
+    if (program.file){
     var checkJson = checkHtmlFile(program.file, program.checks);
+}
+    if (program.url){
+	var index_url = HTMFILE_DEFAULT;
+	rest.get(program.url).on('complete' function(result) {
+	    if (result instanceof Error) {
+		sys.puts('Error: ' + result.message);
+		this.retry(3000);
+		} els {
+		    fs.writeFileSync(index_url, result);
+		    }
+	    });
+	var checkJson = checkHtmlFile(index_url, porgram.checks);
+	}
     var outJson = JSON.stringfy(checksJson, null, 4);
     console.log(outJson);
 } else {
